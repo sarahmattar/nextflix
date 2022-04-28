f96197ab2aedcf7e0838110e20f4772b72715d0b
Sarah Mattar <sarah.mattar@commit.dev>
2022-04-27 20:57:25 -0700

    Init commit for hosting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,67 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useSession, signIn, signOut } from 'next-auth/client'
 
 const Home: NextPage = () => {
+  const [session, loading] = useSession()
+
+  console.log(session, loading)
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Nextflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <main
+        className="flex w-full flex-1 flex-col items-center justify-center bg-black px-20 text-center"
+        style={{
+          backgroundImage:
+            'linear-gradient(rgba(0, 0, 0, 0.9), rgba(135, 80, 156, 0.6)), url(netflix_bg.jpeg)',
+        }}
+      >
+        {!session && (
+          <>
+            Not Signed In <br />
+            <button onClick={() => signIn()}>Sign In</button>
+          </>
+        )}{' '}
+        {
+          <>
+            session &&{' '}
+            <>
+              {/* Signed in as {session.user.email} */}
+              <br />
+              <button onClick={() => signOut()}>Sign Out</button>
+            </>
+          </>
+        }
+        <h1 className="max-w-lg text-5xl font-bold text-white">
+          Unlimited Movies, TV Shows, and more.
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+        <h2 className="my-4 mt-4 mb-8 text-2xl text-white">
+          Watch anywhere, cancel anytime.
+        </h2>
+        <p className="text-white">
+          Ready to watch? Enter your email to create or restart your membership.
         </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+        <div className="mt-4 flex">
+          <input
+            className="width-full bg-white p-4"
+            placeholder="Email Address"
+            type="email"
+            name="email"
+            id="email"
+          />
+          <button
+            className="border-bg-red-700 bg-red-700 px-6 text-lg text-white"
+            type="submit"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Get Started
+          </button>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
