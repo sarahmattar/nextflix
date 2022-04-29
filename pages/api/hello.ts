402eb0c0b482d5e6f0e46bb06cc46b41a8ce4de5
Sarah Mattar <sarah.mattar@commit.dev>
2022-04-28 20:27:47 -0700

    Add protected route, Google sign in

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { Session } from 'next-auth/client'
+import { getSession } from 'next-auth/client'
 
 type Data = {
-  name: string
+  content: string | Session | {}
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+type Session = {
+  user?: {
+    name?: string
+    email?: string
+    image?: string
+  }
+  expires?: string
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const session = await getSession({ req })
+  console.log(session)
+
+  if (session) {
+    res.send({ content: session })
+  } else {
+    res.send({ content: 'You must sign in' })
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { useSession, signIn, signOut } from 'next-auth/client'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
   const [session, loading] = useSession()
+  const router = useRouter()
 
   console.log(session, loading)
+  useEffect(() => {
+    router.push('/app')
+  }, [])
+
+  const SignInStatus = () => {
+    if (session) {
+      return (
+        <>
+          <p className="text-white">Signed in as {session?.user?.name}</p>
+          <button className="text-white" onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </>
+      )
+    }
+    return (
+      <>
+        <p className="text-white">Not Signed In.</p>
+        <button className="text-white" onClick={() => signIn()}>
+          Sign in here
+        </button>
+      </>
+    )
+  }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -21,22 +48,7 @@ const Home: NextPage = () => {
             'linear-gradient(rgba(0, 0, 0, 0.9), rgba(135, 80, 156, 0.6)), url(netflix_bg.jpeg)',
         }}
       >
-        {!session && (
-          <>
-            Not Signed In <br />
-            <button onClick={() => signIn()}>Sign In</button>
-          </>
-        )}{' '}
-        {
-          <>
-            session &&{' '}
-            <>
-              {/* Signed in as {session.user.email} */}
-              <br />
-              <button onClick={() => signOut()}>Sign Out</button>
-            </>
-          </>
-        }
+        <SignInStatus />
         <h1 className="max-w-lg text-5xl font-bold text-white">
           Unlimited Movies, TV Shows, and more.
         </h1>
@@ -56,7 +68,7 @@ const Home: NextPage = () => {
           />
           <button
             className="border-bg-red-700 bg-red-700 px-6 text-lg text-white"
-            type="submit"
+            onClick={() => signIn()}
           >
             Get Started
           </button>
